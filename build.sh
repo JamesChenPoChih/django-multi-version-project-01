24463e7fd29be7790f46404169a62ebd0e0f8fd1
JamesChen <JamesChenPoChih@users.noreply.github.com>
2025-09-16 22:31:49 -0600

    Build: Add submodule update and debug file listing

--- build.sh
+++ build.sh
@@ -2,6 +2,14 @@
 # exit on error
 set -o errexit
 
+# Add submodule update
+git submodule update --init --recursive
+
+# Add for debugging
+echo "--- Listing all files for debugging ---"
+ls -R
+echo "--- End of file listing ---"
+
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
